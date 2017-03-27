236f1b0d32a89c864f44c6c0a3568a8c5f00139b
David Barrat <david.barrat.1@gmail.com>
2017-03-27 19:15:52 +0200

    Update sources

--- david_ionic/www/js/controllers.js
+++ david_ionic/www/js/controllers.js
@@ -1,51 +1,110 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic'])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $ionicModal) {
 
   $scope.tasks = [];
+  $scope.cart = [];
 
-  $scope.addTask = function()
+  $scope.editLabel = "";
+  $scope.editAmount = "";
+  $scope.selectedIndex = "";
+
+  $scope.addTask = function(task)
   {
-    var newTaskObj = 
+
+    if(task.label.length > 0 && task.amount.length > 0)
     {
-      labelTask:"New Task"+$scope.tasks.length,
-      amount:"amount"+$scope.tasks.length
+
+      // Checking types later here for amount
+      var newTaskObj =
+      {
+        label:task.label,
+        amount:task.amount
+      }
+
+      $scope.tasks.push(newTaskObj)
+      $scope.modal.hide();
     }
 
-    $scope.tasks.push(newTaskObj)
+    document.getElementById('newLabelID').value = "";
+    document.getElementById('newLabelAmount').value = "";
+
+
   }
 
+  // Remove a task
   $scope.removeTask  = function(id)
   {
-
-    if (id > -1) {
-      $scope.tasks.splice(id, 1);
-    }
+     $scope.tasks.splice(id, 1);
   }
 
+  $scope.addToCart = function(id)
+  {
+    // Let's assume id exists and is an integer
+    var tmpItem = $scope.tasks[id]
+    $scope.cart.push(tmpItem);
+    $scope.removeTask(id);
+  }
+
+
+  $scope.editExistingTask = function(id)
+  {
+    // Let's assume id exists and is an integer
+    var tmpItem = $scope.tasks[id]
+    $scope.editLabel = tmpItem.label
+    $scope.editAmount = tmpItem.amount
+    $scope.selectedIndex = id;
+    $scope.viewEditTask()
+  }
+
+  $scope.editTask = function()
+  {
+    // Let's assume id exists and is an integer
+    $scope.tasks[$scope.selectedIndex].label =  document.getElementById('editLabelID').value;
+    $scope.tasks[$scope.selectedIndex].amount = document.getElementById('editLabelAmount').value;
+    $scope.modal_edit.hide();
+    $scope.editLabel = "";
+    $scope.editAmount = "";
+    $scope.selectedIndex = "";
+  }
+
+
+  // Modal initilization
+  $ionicModal.fromTemplateUrl('templates/modal.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  })
+
+  $ionicModal.fromTemplateUrl('templates/modal_edit.html', {
+    scope: $scope
+  }).then(function(modal_edit) {
+    $scope.modal_edit = modal_edit;
+  })
+
+
+  $ionicModal.fromTemplateUrl('templates/view_cart.html', {
+    scope: $scope
+  }).then(function(view_cart) {
+    $scope.view_cart = view_cart;
+  })
+
+
+  // Modal display functions
+  $scope.createNewTask = function(e)
+  {
+    $scope.modal.show();
+  }
+
+  $scope.viewEditTask = function(e)
+  {
+    $scope.modal_edit.show();
+  }
+
+  $scope.viewCart = function(e)
+  {
+    $scope.view_cart.show();
+  }
+
+
 })
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
